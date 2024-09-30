ceedbbc0815d1ca49dd9a61c186c990a823a2dbc
Aditya <aditya.vishwabharati@gmail.com>
2024-09-30 15:54:48 +0530

    routing done

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Landing from './pages/Landing';
+import Results from './pages/Results';
+import VendorDetails from './pages/VendorDetails';
+import Profile from './pages/Profile';
+import AddVendor from './pages/AddVendor';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/vendor/:id" element={<VendorDetails />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/add-vendor" element={<AddVendor />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
