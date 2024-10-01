a5ec42a5102d35d5e0626ce9b47e9a33cad96720
Aditya <aditya.vishwabharati@gmail.com>
2024-10-02 01:10:47 +0530

    updated routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import AddVendor from './pages/AddVendor';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Header from './components/Header';
+import Admin from './pages/Admin';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path="/add-vendor" element={<AddVendor />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </Router>
   );
